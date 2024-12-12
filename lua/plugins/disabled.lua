67b7d573dc58cfbcd46bb1559fc6fa3fd53b9ea3
Anton Khodak <khodak@fastmail.com>
2024-12-12 19:47:20 +0100

    up

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,1 +1,7 @@
-return {}
+return {
+  { "hrsh7th/nvim-cmp", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "zbirenbaum/copilot.lua", enabled = false },
+  -- { "neovim/nvim-lspconfig", enabled = false },
+}
